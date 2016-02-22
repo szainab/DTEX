21857fb2dcf0636516f7df483e5d35efd2f713b6
szainab <rimshazainab@hotmail.com>
2016-02-21 22:03:11 -0500

    Leap.py is in lib folder

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/x64' if sys.maxsize > 2**3 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
+sys.path.append('lib')
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
