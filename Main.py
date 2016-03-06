71017eb2d7f1945928dc972a30d501ba9fc0a7a6
szainab <rimshazainab@hotmail.com>
2016-03-05 21:35:23 -0500

    Changed a bunch of things

--- Main.py
+++ Main.py
@@ -35,98 +35,19 @@ def detectSwipe(frame, prevFrame):
             break
 
     return
-'''
-def is_d(frame):
-    hand = frame.hands[0]
 
-    #get a list of only the extended fingers
-    ext_fingers = hand.fingers.extended()
-
-    #if there is only one extended finger and it is the index finger
-    if (len(ext_fingers) == 1 and ext_fingers[0].type == "TYPE_INDEX"):
-
-        #get the floored x,y,z vals for finger direction
-        x = math.floor(ext_fingers[0].direction.x)
-        y = math.floor(ext_fingers[0].direction.y)
-        z = math.floor(ext_fingers[0].direction.z)
-        if (x == 0 and abs(y) == 1 and z == 0):
-            return True
-        else: return False
-    else:
-        return False
-
-def is_l(frame):
-    hand = frame.hands[0]
-
-    #list of extended fingers
-    ext_fingers = hand.fingers.extended()
-    ext_fingers_types = [finger.type for finger in ext_fingers]
-
-    #Check length of ext_fingers and types. 
-    #Unsure of syntax, need to check. 
-    if (len(ext_fingers) == 2 and "TYPE_THUMB" in ext_fingers_types and "TYPE_INDEX" in ext_fingers_types):
-	thumb = 0
-	index = 1
-	#if the fingers are thumb and index
-	if (ext_fingers[1].type == "TYPE_THUMB"):
-	    thumb = 1
-	    index = 0
-        
-	#Set co-ordinates for thumb and finger
-	#Floor would just take it down to zero??
-	x_thumb = int(round(ext_fingers[thumb].direction.x))
-	y_thumb = int(round(ext_fingers[thumb].direction.y))
-	z_thumb = int(round(ext_fingers[thumb].direction.z))
-
-	x_index = int(round(ext_fingers[index].direction.x))
-	y_index = int(round(ext_fingers[index].direction.y))
-	z_index = int(round(ext_fingers[index].direction.z))
-	
-	if(abs(x_thumb) == 1 and y_thumb == 0 and z_thumb == 0
-			and x_index == 0 and abs(y_index) == 1 and z_index == 0):
-	    return True
-        else:
-	    return False
- 
-
-    else:
-        return False
-
-def is_w(frame):
-	hand = frame.hands[0]
-	
-	#get a list of only the extended fingers
-	ext_fingers = hand.fingers.extended()
-	ext_fingers_types = [finger.type for finger in ext_fingers]
-	
-	# if there are three extended fingers and the fingers types are index, middle and ring
-	if (len(ext_fingers) == 3 and ("TYPE_INDEX" in ext_fingers_types) 
-		and ("TYPE_MIDDLE" in ext_fingers_types) and ("TYPE_RING" in ext_fingers_types):
-		#floored x,y,z values for first extended finger
-		x1 = math.floor(ext_fingers[0].direction.x)
-        	y1 = math.floor(ext_fingers[0].direction.y)
-        	z1 = math.floor(ext_fingers[0].direction.z)
-		
-		#floored x,y,z values for second extended finger
-		x2 = math.floor(ext_fingers[1].direction.x)
-        	y2 = math.floor(ext_fingers[1].direction.y)
-        	z2 = math.floor(ext_fingers[1].direction.z)
-		
-		#floored x,y,z values for third extended finger
-		x3 = math.floor(ext_fingers[2].direction.x)
-        	y3 = math.floor(ext_fingers[2].direction.y)
-        	z3 = math.floor(ext_fingers[2].direction.z)
-		
-		#x,y,z coordinates for three extended fingers should be (0,1,0)
-		if(x1 == 0 and y1 == 1 and z1 == 0 and x2 == 0 and y2 == 1 
-			and z2 == 0 and x3 == 0 and y3 == 1 and z3 == 0):
-			return True
-		else:
-			return False
-			
+def detectingLetter(frame):
+	if functions.is_l(frame):
+		print "L"
+	elif functions.is_d(frame):
+		print "D"
+	elif functions.is_w(frame):
+		print "W"
+	elif functions.is_h(frame):
+		print "H"
 	else:
-		return False
-'''
+		print "Unidentified Letter"
+
 class SampleListener(Leap.Listener):
     
     def on_connect(self, controller):
@@ -144,8 +65,11 @@ class SampleListener(Leap.Listener):
         prevFrame = controller.frame(1)
 
         detectSwipe(frame,prevFrame)
-        print functions.is_d(frame)
-        # counter = 1
+        #print functions.is_h(frame)
+	#print functions.is_l(frame)
+	#print functions.is_d(frame)
+        detectingLetter(frame)
+	# counter = 1
 		
 		
         # for gesture in frame.gestures():
--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
+#Type of thumb = 0
+#Type of index = 1
+#Type of middle = 2
+#Type of ring = 3
+#Type of pinky = 4
+
+
+
 def reset(frame):
 	hand = frame.hands[0]
 
@@ -22,60 +30,103 @@ def reset(frame):
 		return False
 
 def is_d(frame):
-    hand = frame.hands[0]
+	hand = frame.hands[0]
+	debug = False
+	#get a list of only the extended fingers
+	ext_fingers = hand.fingers.extended()
+	if debug == True:
+    		print "length of array = " + str(len(ext_fingers))
+    		print "types in the array = " + str([finger.type for finger in ext_fingers])
+		#print "types array" + str(ext_fingers_types)
 
-    #get a list of only the extended fingers
-    ext_fingers = hand.fingers.extended()
+    	#if there is only one extended finger and it is the index finger
+    	if (len(ext_fingers) == 1 and ext_fingers[0].type == 1):
 
-    #if there is only one extended finger and it is the index finger
-    if (len(ext_fingers) == 1 and ext_fingers[0].type == "TYPE_INDEX"):
-
-        #get the floored x,y,z vals for finger direction
-        x = math.floor(ext_fingers[0].direction.x)
-        y = math.floor(ext_fingers[0].direction.y)
-        z = math.floor(ext_fingers[0].direction.z)
-        if (x == 0 and abs(y) == 1 and z == 0):
-            return True
-        else: return False
-    else:
-        return False
+        	#get the floored x,y,z vals for finger direction
+		#int ensures no decimal places
+		#round rounds up number to 1 dp 
+        	x = int(round(ext_fingers[0].direction.x))
+        	y = int(round(ext_fingers[0].direction.y))
+        	z = int(round(ext_fingers[0].direction.z))
+        	if (x == 0 and abs(y) == 1 and z == 0):
+            		return True
+        	else: return False
+    	else:
+        	return False
 
 def is_l(frame):
-    hand = frame.hands[0]
+	debug = False
+    	hand = frame.hands[0]
 
-    #list of extended fingers
-    ext_fingers = hand.fingers.extended()
-    ext_fingers_types = [finger.type for finger in ext_fingers]
+    	#list of extended fingers
+    	ext_fingers = hand.fingers.extended()
+    	ext_fingers_types = [finger.type for finger in ext_fingers]
+    
+    	if debug == True:
+    		print "length of array = " + str(len(ext_fingers))
+    		print "types in the array = " + str([finger.type for finger in ext_fingers])
+		print "types array" + str(ext_fingers_types)
+    	#Check length of ext_fingers and types. 
+    	#Unsure of syntax, need to check. 
+	if len(ext_fingers) == 2 and ext_fingers_types[0] == 0 and ext_fingers_types[1] == 1 :
+		if debug == True:
+			print "Inside the function"
+		thumb = 0
+		index = 1
+		        
+		#Set co-ordinates for thumb and finger
+		#Floor would just take it down to zero??
+		x_thumb = int(round(ext_fingers[thumb].direction.x))
+		y_thumb = int(round(ext_fingers[thumb].direction.y))
+		z_thumb = int(round(ext_fingers[thumb].direction.z))
 
-    #Check length of ext_fingers and types. 
-    #Unsure of syntax, need to check. 
-    if (len(ext_fingers) == 2 and "TYPE_THUMB" in ext_fingers_types and "TYPE_INDEX" in ext_fingers_types):
-	thumb = 0
-	index = 1
-	#if the fingers are thumb and index
-	if (ext_fingers[1].type == "TYPE_THUMB"):
-	    thumb = 1
-	    index = 0
-        
-	#Set co-ordinates for thumb and finger
-	#Floor would just take it down to zero??
-	x_thumb = int(round(ext_fingers[thumb].direction.x))
-	y_thumb = int(round(ext_fingers[thumb].direction.y))
-	z_thumb = int(round(ext_fingers[thumb].direction.z))
-
-	x_index = int(round(ext_fingers[index].direction.x))
-	y_index = int(round(ext_fingers[index].direction.y))
-	z_index = int(round(ext_fingers[index].direction.z))
+		x_index = int(round(ext_fingers[index].direction.x))
+		y_index = int(round(ext_fingers[index].direction.y))
+		z_index = int(round(ext_fingers[index].direction.z))
 	
-	if(abs(x_thumb) == 1 and y_thumb == 0 and z_thumb == 0
-			and x_index == 0 and abs(y_index) == 1 and z_index == 0):
-	    return True
-	else:
-	    return False
+		if debug == True:
+			print "thumb values (x y z) " + str(x_thumb) + ", " + str(y_thumb) + ", " + str(z_thumb)
+			print "index values (x y z) " + str(x_index) + ", " + str(y_index) + ", " + str(z_index)
+
+		if(abs(x_thumb) == 1 and y_thumb == 0 and z_thumb == 0
+				and x_index == 0 and abs(y_index) == 1 and z_index == 0):
+	    		return True
+		else:
+	    		return False
  
 
-    else:
-        return False
+    	else:
+        	return False
+
+def is_h(frame):
+	debug = False
+	hand = frame.hands[0]
+	ext_fingers = hand.fingers.extended()
+	#ext_finger_types = [finger.type for finger in ext_fingers]
+	if(debug == True):
+		print "Length of ext_fingers" + str(len(ext_fingers))
+		print "ext_fingers types " + str([finger.type for finger in ext_fingers])
+	if(len(ext_fingers) == 2 and ext_fingers[0].type == 1 and ext_fingers[1].type == 2):
+		if debug == True: 
+			print "Inside the function"
+		x_index = int(round(ext_fingers[0].direction.x))
+		y_index = int(round(ext_fingers[0].direction.y))
+		z_index = int(round(ext_fingers[0].direction.z))
+		
+
+		x_middle = int(round(ext_fingers[1].direction.x))
+		y_middle = int(round(ext_fingers[1].direction.y))
+		z_middle = int(round(ext_fingers[1].direction.z))
+		if debug == True:
+			print "Index values [x,y,z]: " + str(x_index) + ", " + str(y_index) + ", " + str(z_index)
+			print "Middle values [x,y,z]: " + str(x_middle) + ", " + str(y_middle) + ", " + str(z_middle)
+
+		if((abs(x_index) == 1 and y_index == 0 and z_index == 0 and abs(x_middle) == 1 and y_middle == 0 and z_middle == 0) or (x_index == 0 and y_index == 0 and abs(z_index) == 1 and x_middle == 0 and y_middle == 0 and abs(z_middle) == 1)):
+			return True
+		else:
+			return False
+	else:
+		return False
 
 def is_w(frame):
 	hand = frame.hands[0]
@@ -85,22 +136,22 @@ def is_w(frame):
 	ext_fingers_types = [finger.type for finger in ext_fingers]
 	
 	# if there are three extended fingers and the fingers types are index, middle and ring
-	if (len(ext_fingers) == 3 and ("TYPE_INDEX" in ext_fingers_types) 
-		and ("TYPE_MIDDLE" in ext_fingers_types) and ("TYPE_RING" in ext_fingers_types)):
+	if (len(ext_fingers) == 3 and (ext_fingers_types[0] == 1) 
+		and (ext_fingers_types[1] == 2) and (ext_fingers_types[2] == 3)):
 		#floored x,y,z values for first extended finger
-		x1 = math.floor(ext_fingers[0].direction.x)
-        	y1 = math.floor(ext_fingers[0].direction.y)
-        	z1 = math.floor(ext_fingers[0].direction.z)
+		x1 = int(round(ext_fingers[0].direction.x))
+        	y1 = int(round(ext_fingers[0].direction.y))
+        	z1 = int(round(ext_fingers[0].direction.z))
 		
 		#floored x,y,z values for second extended finger
-		x2 = math.floor(ext_fingers[1].direction.x)
-        	y2 = math.floor(ext_fingers[1].direction.y)
-        	z2 = math.floor(ext_fingers[1].direction.z)
+		x2 = int(round(ext_fingers[1].direction.x))
+        	y2 = int(round(ext_fingers[1].direction.y))
+        	z2 = int(round(ext_fingers[1].direction.z))
 		
 		#floored x,y,z values for third extended finger
-		x3 = math.floor(ext_fingers[2].direction.x)
-        	y3 = math.floor(ext_fingers[2].direction.y)
-        	z3 = math.floor(ext_fingers[2].direction.z)
+		x3 = int(round(ext_fingers[2].direction.x))
+        	y3 = int(round(ext_fingers[2].direction.y))
+        	z3 = int(round(ext_fingers[2].direction.z))
 		
 		#x,y,z coordinates for three extended fingers should be (0,1,0)
 		if(x1 == 0 and y1 == 1 and z1 == 0 and x2 == 0 and y2 == 1 
