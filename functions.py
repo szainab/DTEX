e86ef360677381628ca21c9054f49256f73c8421
bemailloux <bemailloux@gmail.com>
2016-03-08 21:30:38 -0500

    converted spaces to tabs

--- functions.py
+++ functions.py
@@ -55,15 +55,15 @@ def is_l(frame):
 	ext_fingers_types = [finger.type for finger in ext_fingers]
 	
 	# if debug:
-    	# 	print "length of array = " + str(len(ext_fingers))
-    	# 	print "types in the array = " + str([finger.type for finger in ext_fingers])
+		# 	print "length of array = " + str(len(ext_fingers))
+		# 	print "types in the array = " + str([finger.type for finger in ext_fingers])
 	# 	print "types array" + str(ext_fingers_types)
-    
+
 	#Check length of ext_fingers and types. 
-    	if len(ext_fingers) == 2 and ext_fingers_types[0] == 0 and ext_fingers_types[1] == 1 :
-	    # if debug == True:
-	    #     print "Inside the function"
-			
+	if len(ext_fingers) == 2 and ext_fingers_types[0] == 0 and ext_fingers_types[1] == 1 :
+		# if debug == True:
+		#     print "Inside the function"
+
 		#Set co-ordinates for thumb and finger
 		#Floor would just take it down to zero??
 		x_thumb = int(round(ext_fingers[0].direction.x))
@@ -73,24 +73,24 @@ def is_l(frame):
 		x_index = int(round(ext_fingers[1].direction.x))
 		y_index = int(round(ext_fingers[1].direction.y))
 		z_index = int(round(ext_fingers[1].direction.z))
-		
+
 		if debug:
 			print "thumb values (x y z) " + str(x_thumb) + ", " + str(y_thumb) + ", " + str(z_thumb)
 			print "index values (x y z) " + str(x_index) + ", " + str(y_index) + ", " + str(z_index)
 
 		if(abs(x_thumb) == 1 and y_thumb == 0 and z_thumb == 0
 				and x_index == 0 and abs(y_index) == 1 and z_index == 0):
-	    		return True
+				return True
 		else:
-	    		return False
+				return False
  
 
-    	else:
-        	return False
+	else:
+		return False
 
 def is_h(frame):
 	# debug = True
-    	hand = frame.hands[0]
+	hand = frame.hands[0]
 	ext_fingers = hand.fingers.extended()
 	#ext_finger_types = [finger.type for finger in ext_fingers]
 	
@@ -104,7 +104,6 @@ def is_h(frame):
 		x_index = int(round(ext_fingers[0].direction.x))
 		y_index = int(round(ext_fingers[0].direction.y))
 		z_index = int(round(ext_fingers[0].direction.z))
-		
 
 		x_middle = int(round(ext_fingers[1].direction.x))
 		y_middle = int(round(ext_fingers[1].direction.y))
@@ -121,7 +120,7 @@ def is_h(frame):
 		return False
 
 def is_w(frame):
-    	# debug = True
+		# debug = True
 	hand = frame.hands[0]
 	
 	#get a list of only the extended fingers
@@ -129,22 +128,21 @@ def is_w(frame):
 	ext_fingers_types = [finger.type for finger in ext_fingers]
 	
 	# if there are three extended fingers and the fingers types are index, middle and ring
-	if (len(ext_fingers) == 3 and (ext_fingers_types[0] == 1) 
-		and (ext_fingers_types[1] == 2) and (ext_fingers_types[2] == 3)):
+	if (len(ext_fingers) == 3 and (ext_fingers_types[0] == 1) and (ext_fingers_types[1] == 2) and (ext_fingers_types[2] == 3)):
 		#floored x,y,z values for first extended finger
 		x1 = int(round(ext_fingers[0].direction.x))
-        	y1 = int(round(ext_fingers[0].direction.y))
-        	z1 = int(round(ext_fingers[0].direction.z))
+		y1 = int(round(ext_fingers[0].direction.y))
+		z1 = int(round(ext_fingers[0].direction.z))
 		
 		#floored x,y,z values for second extended finger
 		x2 = int(round(ext_fingers[1].direction.x))
-        	y2 = int(round(ext_fingers[1].direction.y))
-        	z2 = int(round(ext_fingers[1].direction.z))
+		y2 = int(round(ext_fingers[1].direction.y))
+		z2 = int(round(ext_fingers[1].direction.z))
 		
 		#floored x,y,z values for third extended finger
 		x3 = int(round(ext_fingers[2].direction.x))
-       		y3 = int(round(ext_fingers[2].direction.y))
-        	z3 = int(round(ext_fingers[2].direction.z))
+		y3 = int(round(ext_fingers[2].direction.y))
+		z3 = int(round(ext_fingers[2].direction.z))
 
 		if debug == True:
 			print "Thumb values [x,y,z]: " + str(x1) + ", " + str(y1) + ", " + str(z1)
