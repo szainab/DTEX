4434304fd48723684955ed5b28fbb96b876245a4
szainab <rimshazainab@hotmail.com>
2016-03-11 10:04:49 -0500

    Clean up + new functions for b and g because I was bored

--- functions.py
+++ functions.py
@@ -1,8 +1,24 @@
+#Fingers:
 #Type of thumb = 0
 #Type of index = 1
 #Type of middle = 2
 #Type of ring = 3
 #Type of pinky = 4
+
+#Bones:
+#Note: Tip of finger is Distal & Thumb has no metacarpal
+#Type of Metacarpal = 0
+#Type of Proximal = 1
+#Type of Intermediate = 2
+#Type of Distal = 3
+
+#The letter b requires a lot of checking the array for the same letter
+#I got bored of writing it so i made a function
+def checkArray(arr, num):
+	if all(item == num for item in arr):
+		return True
+	else:
+		return False
 
 def reset(frame):
 	hand = frame.hands[0]
@@ -19,9 +35,25 @@ def reset(frame):
 			  int(round(ext_fingers[2].direction.y)),
 			  int(round(ext_fingers[3].direction.y)),
 			  int(round(ext_fingers[4].direction.y))]
-		if all(y == 0 for y in y_val):
+		if checkArray(y_val, 0):
 			return True 	#only return True in this case, return False by default
 
+	return False
+
+def is_b(frame):
+	hand = frame.hands[0]
+	#list of extended fingers:
+	ext_fingers = hand.fingers.extended()
+
+	#If there are four extended fingers and thumb is not one of them
+	if(len(ext_fingers) == 4 and ext_fingers[0] != 0):
+		#rounded co-ordinates for all fingers, stored in respective arrays
+		x = [int(round(ext_fingers[0].direction.x)), int(round(ext_fingers[1].direction.x)), int(round(ext_fingers[2].direction.x)), int(round(ext_fingers[3].direction.x))]
+		y = [abs(int(round(ext_fingers[0].direction.y))), abs(int(round(ext_fingers[1].direction.y))), abs(int(round(ext_fingers[2].direction.y))), abs(int(round(ext_fingers[3].direction.y)))]
+		z = [int(round(ext_fingers[0].direction.z)), int(round(ext_fingers[1].direction.z)), int(round(ext_fingers[2].direction.z)), int(round(ext_fingers[3].direction.z))]
+
+		if (checkArray(x, 0) and checkArray(y, 1) and checkArray(z, 0)):
+			return True #Only return True in this case, return False by default
 	return False
 
 def is_d(frame):
@@ -42,9 +74,9 @@ def is_d(frame):
 		y = int(round(ext_fingers[0].direction.y))
 		z = int(round(ext_fingers[0].direction.z))
 		if debug: print "D " + str(x) + ", " + str(y) + ", " + str(z)
-		if (x == 0 and abs(y) == 1 and z == 0): return True
-		else: return False
-	else: return False
+		if (x == 0 and abs(y) == 1 and z == 0):
+			return True #Only True in this case, returns False by default
+	return False
 
 def is_l(frame):
 	# debug = True
@@ -83,13 +115,8 @@ def is_l(frame):
 
 		if(abs(x_thumb) == 1 and y_thumb == 0 and z_thumb == 0
 				and x_index == 0 and abs(y_index) == 1 and z_index == 0):
-				return True
-		else:
-				return False
- 
-
-	else:
-		return False
+				return True #Only True in this case, return False by default
+	return False
 
 def is_h(frame):
 	# debug = True
@@ -113,17 +140,41 @@ def is_h(frame):
 		z_middle = int(round(ext_fingers[1].direction.z))
 
 		x_palm = int(round(hand.palm_normal.x))
-		if debug == True:
+		if debug:
 			print "Index values [x,y,z]: " + str(x_index) + ", " + str(y_index) + ", " + str(z_index)
 			print "Middle values [x,y,z]: " + str(x_middle) + ", " + str(y_middle) + ", " + str(z_middle)
 			print hand.palm_normal
 
 		if((abs(x_index) == 1 and y_index == 0 and z_index == 0 and abs(x_middle) == 1 and y_middle == 0 and z_middle == 0) or (x_index == 0 and y_index == 0 and abs(z_index) == 1 and x_middle == 0 and y_middle == 0 and abs(z_middle) == 1 and abs(x_palm) == 1)):
-			return True
-		else:
-			return False
-	else:
-		return False
+			return True #only true in this case, returns false by default
+	return False
+
+def is_g(frame):
+	# debug = True
+	hand = frame.hands[0]
+	ext_fingers = hand.fingers.extended()
+	#ext_finger_types = [finger.type for finger in ext_fingers]
+
+	# if(debug == True):
+	# 	print "Length of ext_fingers" + str(len(ext_fingers))
+	# 	print "ext_fingers types " + str([finger.type for finger in ext_fingers])
+
+	if(len(ext_fingers) == 1 and ext_fingers[0].type == 1):
+		# if debug == True:
+		# 	print "Inside the function"
+		x_index = int(round(ext_fingers[0].direction.x))
+		y_index = int(round(ext_fingers[0].direction.y))
+		z_index = int(round(ext_fingers[0].direction.z))
+
+		x_palm = int(round(hand.palm_normal.x))
+
+		if debug:
+			print "Index values [x,y,z]: " + str(x_index) + ", " + str(y_index) + ", " + str(z_index)
+			print hand.palm_normal
+
+		if((abs(x_index) == 1 and y_index == 0 and z_index == 0) or (x_index == 0 and y_index == 0 and abs(z_index) == 1 and abs(x_palm) == 1)):
+			return True #only true in this case, returns false by default
+	return False
 
 def is_w(frame):
 		# debug = True
@@ -158,10 +209,6 @@ def is_w(frame):
 		#x,y,z coordinates for three extended fingers should be (0,1,0)
 		if(x1 == 0 and y1 == 1 and z1 == 0 and x2 == 0 and y2 == 1 
 			and z2 == 0 and x3 == 0 and y3 == 1 and z3 == 0):
-			return True
-		else:
-			return False
-			
-	else:
-		return False
+			return True	#only returns true in this case, returns false by default
+	return False
 
